9dcca2d3fa83c8d615f8604018b8dd7404a3973e
John Plantamura <plantamurajk@gmail.com>
2019-06-16 23:35:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
-function destructivelyRemoveElementfromBeginningOfArray(array){
-  array = array.shift()
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
   return array
 }
