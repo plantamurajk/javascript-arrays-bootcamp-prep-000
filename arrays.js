89d92a320a50b7c9853b097348cb6d0fa010514d
John Plantamura <plantamurajk@gmail.com>
2019-06-16 23:33:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
+
+function destructivelyRemoveElementfromBeginningOfArray(array){
+  array.shift()
+  return array
+}
